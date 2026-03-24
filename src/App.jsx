bbac284f5f137dce8d335420427d2e0b5a9f5709
Pavalanganesan2368 <pavalanganesan2368@gmail.com>
2026-03-24 10:43:54 +0530

    Project Started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='min-screen h-screen flex justify-center items-center bg-sky-200'>
+        <h1 className='text-3xl text-sky-500 font-bold'>Placement Project</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
